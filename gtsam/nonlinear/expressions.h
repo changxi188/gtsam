fe70cf10f84e3114027f66a7f16d07c427ebe347
changdada188 <1067720919@qq.com>
2023-05-30 02:33:56 +0800

    Add: format

--- gtsam/nonlinear/expressions.h
+++ gtsam/nonlinear/expressions.h
@@ -7,25 +7,28 @@
 
 #pragma once
 
-#include <gtsam/nonlinear/Expression.h>
 #include <gtsam/base/Lie.h>
+#include <gtsam/nonlinear/Expression.h>
 
-namespace gtsam {
+namespace gtsam
+{
 
 // Generic between, assumes existence of traits<T>::Between
 template <typename T>
-Expression<T> between(const Expression<T>& t1, const Expression<T>& t2) {
-  return Expression<T>(traits<T>::Between, t1, t2);
+Expression<T> between(const Expression<T>& t1, const Expression<T>& t2)
+{
+    return Expression<T>(traits<T>::Between, t1, t2);
 }
 
 // Generic compose, assumes existence of traits<T>::Compose
 template <typename T>
-Expression<T> compose(const Expression<T>& t1, const Expression<T>& t2) {
-  return Expression<T>(traits<T>::Compose, t1, t2);
+Expression<T> compose(const Expression<T>& t1, const Expression<T>& t2)
+{
+    return Expression<T>(traits<T>::Compose, t1, t2);
 }
 
 // Some typedefs
-typedef Expression<double> Double_;
+typedef Expression<double>  Double_;
 typedef Expression<Vector1> Vector1_;
 typedef Expression<Vector2> Vector2_;
 typedef Expression<Vector3> Vector3_;
@@ -36,4 +39,4 @@ typedef Expression<Vector7> Vector7_;
 typedef Expression<Vector8> Vector8_;
 typedef Expression<Vector9> Vector9_;
 
-}  // \namespace gtsam
+}  // namespace gtsam
