f37a4a2214371ec8837bb6d728c44e682bad7daf
贝叶斯-成昌喜 <chengchangxi@holomatic.com>
2023-09-06 14:01:57 +0800

    Add: construct factor graph as isam2 paper

--- examples/self_test/self_test_isam2_bayes_tree.cpp
+++ examples/self_test/self_test_isam2_bayes_tree.cpp
@@ -8,28 +8,41 @@
 
 int main()
 {
+    gtsam::Key                               L1 = gtsam::symbol('L', 1);
+    gtsam::Key                               L2 = gtsam::symbol('L', 2);
+    gtsam::Pose3                             pt1(gtsam::Rot3::Identity(), gtsam::Point3(0, 1, 0));
+    gtsam::Pose3                             pt2(gtsam::Rot3::Identity(), gtsam::Point3(2, 1, 0));
     gtsam::Key                               X1 = gtsam::symbol('X', 1);
     gtsam::Key                               X2 = gtsam::symbol('X', 2);
     gtsam::Key                               X3 = gtsam::symbol('X', 3);
     gtsam::Pose3                             p1(gtsam::Rot3::Identity(), gtsam::Point3::Zero());
     gtsam::Pose3                             p2(gtsam::Rot3::Identity(), gtsam::Point3(1, 0, 0));
     gtsam::Pose3                             p3(gtsam::Rot3::Identity(), gtsam::Point3(2, 0, 0));
-    gtsam::Pose3                             measure(gtsam::Rot3::Identity(), gtsam::Point3(1, 0, 0));
+    gtsam::Pose3                             p_measure(gtsam::Rot3::Identity(), gtsam::Point3(1, 0, 0));
+    gtsam::Pose3                             l_measure1 = gtsam::traits<gtsam::Pose3>::Between(pt1, p1);
+    gtsam::Pose3                             l_measure2 = gtsam::traits<gtsam::Pose3>::Between(pt1, p2);
+    gtsam::Pose3                             l_measure3 = gtsam::traits<gtsam::Pose3>::Between(pt2, p3);
     gtsam::noiseModel::Isotropic::shared_ptr noise_mode = gtsam::noiseModel::Isotropic::Sigma(6, 0.01);
 
     gtsam::PriorFactor<gtsam::Pose3>   prior_factor(X1, p1, noise_mode);
-    gtsam::PriorFactor<gtsam::Pose3>   prior_factor_x2(X2, p2, noise_mode);
-    gtsam::BetweenFactor<gtsam::Pose3> between_factor_12(X1, X2, measure, noise_mode);
-    gtsam::BetweenFactor<gtsam::Pose3> between_factor_23(X2, X3, measure, noise_mode);
+    gtsam::BetweenFactor<gtsam::Pose3> between_factor_12(X1, X2, p_measure, noise_mode);
+    gtsam::BetweenFactor<gtsam::Pose3> between_factor_23(X2, X3, p_measure, noise_mode);
+    gtsam::BetweenFactor<gtsam::Pose3> between_factor_l1_X1(L1, X1, l_measure1, noise_mode);
+    gtsam::BetweenFactor<gtsam::Pose3> between_factor_l1_X2(L1, X2, l_measure2, noise_mode);
+    gtsam::BetweenFactor<gtsam::Pose3> between_factor_l2_X3(L2, X3, l_measure3, noise_mode);
 
     gtsam::Values values;
+    values.insert(L1, pt1);
+    values.insert(L2, pt2);
     values.insert(X1, p1);
     values.insert(X2, p2);
     values.insert(X3, p3);
 
     gtsam::NonlinearFactorGraph nonlinear_factor_graph;
+    nonlinear_factor_graph.add(between_factor_l1_X1);
+    nonlinear_factor_graph.add(between_factor_l1_X2);
+    nonlinear_factor_graph.add(between_factor_l2_X3);
     nonlinear_factor_graph.add(prior_factor);
-    nonlinear_factor_graph.add(prior_factor_x2);
     nonlinear_factor_graph.add(between_factor_12);
     nonlinear_factor_graph.add(between_factor_23);
     nonlinear_factor_graph.print("nonlinear_factor_graph : ");
@@ -44,6 +57,11 @@ int main()
     gtsam::VariableIndex affectedFactorsVarIndex(*gaussian_factor_graph);
     affectedFactorsVarIndex.print("Affected var Factors index : ");
     gtsam::FastMap<gtsam::Key, int> constraintGroups;
+    constraintGroups.emplace(L1, 0);
+    constraintGroups.emplace(L2, 1);
+    constraintGroups.emplace(X1, 2);
+    constraintGroups.emplace(X2, 3);
+    constraintGroups.emplace(X3, 4);
     const gtsam::Ordering ordering = gtsam::Ordering::ColamdConstrained(affectedFactorsVarIndex, constraintGroups);
     ordering.print("colamd ordering : ");
 
